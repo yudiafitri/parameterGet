3224191c6ebfa46565e495c6dee3d3febec7e3f7
yudiafitri <49948179+yudiafitri@users.noreply.github.com>
2019-04-24 22:31:33 +0800

    First Commit
    First Commit

---
+++ hubungi.php
@@ -0,0 +1,5 @@
+<center>
+	<h2>Hubungi saya di</h2><br>
+		<i class="glyphicon glyphicon-envelope"></i> 175500@smktelkommedan.sch.id<br>
+		<i class="glyphicon glyphicon-phone"></i> <a href="">08219849125</a>
+</center>
